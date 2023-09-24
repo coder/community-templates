70d070c02632d886e61128fbd0d3b24883893c17
Ben <me@bpmct.net>
2023-09-24 11:53:43 +0000

    feat: add sample structure & template

---
+++ .sample/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    coder = {
+      source = "coder/coder"
+      # See latest version: https://registry.terraform.io/providers/coder/coder
+      version = ">= 0.12.0"
+    }
+  }
+}
+
+# This example does not provision any resources. Use this
+# template as a starting point for writing custom templates
+# using any Terraform resource/provider.
+#
+# See: https://coder.com/docs/coder-oss/latest/templates
+
+data "coder_workspace" "me" {
+}
+
+resource "coder_agent" "dev1" {
+  os   = "linux"
+  arch = "amd64"
+  auth = "token"
+}
+
+resource "null_resource" "fake-compute" {
+  # When a workspace is stopped, this resource is destroyed.
+  count = data.coder_workspace.me.transition == "start" ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "echo 🔊 ${data.coder_workspace.me.owner} has started a workspace named ${data.coder_workspace.me.name}. Template: MODULE_NAME"
+  }
+
+  # Run the Coder agent init script on resources
+  # to access web apps and SSH:
+  #
+  # #!/bin/sh
+  # export CODER_AGENT_TOKEN=${coder_agent.dev1.token}
+  # ${coder_agent.dev1.init_script}
+}
+
+resource "null_resource" "fake-disk" {
+  # This resource will remain even when workspaces are restarted.
+  count = 1
+}
+
+resource "coder_app" "fake-app" {
+  # Access :8080 in the workspace from the Coder dashboard.
+  name     = "VS Code"
+  icon     = "/icon/code.svg"
+  agent_id = coder_agent.dev1.id
+  url      = "http://localhost:8080"
+}
